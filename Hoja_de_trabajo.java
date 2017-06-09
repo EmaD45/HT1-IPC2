c8668308cebca03c6bbe9bb52636315401847e56
EmaD45 <elba1995@gmail.com>
2017-06-08 19:25:36 -0600

    Hipotenusa
    Programa que brinda el resultado de la hipotenusa ingresando cateto A y B.

---
+++ Hoja_de_trabajo.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hoja_de_trabajo;
+
+/**
+ *
+ * @author Elba
+ */
+public class Hoja_de_trabajo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        ProgramaHT pro = new ProgramaHT();
+        pro.setVisible(true);
+    }
+    
+}
